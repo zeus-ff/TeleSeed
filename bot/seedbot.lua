511c0dd4828890fdfc3dae23f3e0776d34d38e31
zeus-ff <tofaniyam@gmail.com>
2016-04-13 17:22:39 +0100

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -237,7 +237,6 @@ function create_config( )
     about_text = [[Teleseed v4
 An advanced administration bot based on TG-CLI written in Lua
 
-https://github.com/SEEDTEAM/TeleSeed
 
 Admins
 @iwals [Founder]
